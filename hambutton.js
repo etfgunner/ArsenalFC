8313b664b5d3502a76cd665b9341e30f3e88df86
etfgunner <milan.zuza18@gmail.com>
2017-01-13 17:37:40 +0100

    -

--- hambutton.js
+++
@@ -1,8 +0,0 @@
-function showMenu() {
-    var x = document.getElementById("menu");
-    if (x.className === "topmenu") {
-        x.className += " short";
-    } else {
-        x.className = "topmenu";
-    }
-}
